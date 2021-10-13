e27a434058f41eb742054a216d160d9dcb647859
Pratt Vaidya <pratt.vaidya@ttt.studio>
2021-10-12 17:26:18 -0700

    Display a ListView with an ArrayAdapter

--- app/src/main/java/ttt/prattvaidya/listviewdemo/MainActivity.java
+++ app/src/main/java/ttt/prattvaidya/listviewdemo/MainActivity.java
@@ -3,6 +3,10 @@ package ttt.prattvaidya.listviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView myListView = findViewById(R.id.listView);
+        ArrayList<String> family = new ArrayList<String>();
+        family.add("Pratt Vaidya");
+        family.add("Deepti Vaidya");
+        family.add("Advait Vaidya");
+        family.add("Ritesh Vaidya");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, family);
+        myListView.setAdapter(arrayAdapter);
     }
 }
