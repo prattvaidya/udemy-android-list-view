0bc86d28b3db0362cf9d35a49e42880b2dea1bf4
Pratt Vaidya <pratt.vaidya@ttt.studio>
2021-10-12 17:32:12 -0700

    Log list items on tap

--- app/src/main/java/ttt/prattvaidya/listviewdemo/MainActivity.java
+++ app/src/main/java/ttt/prattvaidya/listviewdemo/MainActivity.java
@@ -3,6 +3,9 @@ package ttt.prattvaidya.listviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -24,5 +27,12 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, family);
         myListView.setAdapter(arrayAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("Person selected", family.get(position));
+            }
+        });
     }
 }
